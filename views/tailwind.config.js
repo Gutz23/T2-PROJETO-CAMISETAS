91826c44cacc3e2b9c39f59b984e421070cdd76c
chaos <brunognveiga@gmail.com>
2024-12-18 15:47:32 -0300

    :sparkles: Feat: Adiciona Estilização da página e imagens do carrosel

--- views/tailwind.config.js
+++ views/tailwind.config.js
@@ -2,9 +2,14 @@
 module.exports = {
     content: ["./views/**/*.{html,js,ejs}", "./node_modules/flowbite/**/*.js"],
         theme: {
-            extend: {},
+            extend: {
+        },
+        fontFamily: {
+            poppins: ["Poppins", "sans-serif"],
+            bree: ["BreeSerif", "serif"],
         },
     plugins: [
         require('flowbite/plugin')
-    ],
-};
+            ],
+        },
+    };
