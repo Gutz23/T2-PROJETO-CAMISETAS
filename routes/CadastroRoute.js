b76ec2b1290cdadcff3a45ed0e85f90e8a5bb9d8
Giovanna Gutierrez <g.gutierrez2303@gmail.com>
2024-12-19 00:14:23 -0300

    :bug: Fix: Corrige botão da navbar

--- routes/CadastroRoute.js
+++ routes/CadastroRoute.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const { cadastrarUser } = require("../controllers/CadastroController");
+const { cadastrarUser } = require('../controllers/cadastroController');
 
 router.get("/cadastro", (req, res) => {
     res.render('cadastro', { error: null });
